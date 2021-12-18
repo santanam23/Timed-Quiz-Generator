2f34438908838c771de265a3cc7735275d47a254
santanam23 <mirmir11.ms@gmail.com>
2021-12-18 17:02:50 -0600

    created html,css,style sheets

---
+++ assets/css/script.js
@@ -0,0 +1,20 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElements = document.getElementById
+('question-container')
+
+startButton.addEventListener('click', startGame)
+
+
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+}
+
+function setNextQuestion() {
+
+
+}
+
+function selectAnswer() {
+
+}
